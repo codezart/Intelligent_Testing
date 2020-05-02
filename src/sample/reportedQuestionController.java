87f60debe1778f394f0509a03c3bb6347daa067c
F5SH123 <39929955+F5SH123@users.noreply.github.com>
2020-05-02 23:23:10 +0300

    Add files via upload

---
+++ src/sample/reportedQuestionController.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class reportedQuestionController {
+
+    private int numReportsResolved;
+    private int adminID;
+
+    public void deleteReportedQuestion(int id){
+
+    }
+    public void createReportedQuestionList(String category){
+
+    }
+    public void modifyReportedQuestion(String val){
+
+    }
+    public void modifyCorrcetAnswers(String val[]){
+
+    }
+    public void modifyType(int val){
+
+    }
+    public void modifyOptions(String val[]){
+
+    }
+
+}
