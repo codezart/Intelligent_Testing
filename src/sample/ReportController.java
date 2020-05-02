844080bb45941a7911b2f1dcdf665784fe85d80d
Mohammed Abdur Rahman <44206264+codezart@users.noreply.github.com>
2020-05-02 23:30:12 +0300

    Add files via upload

---
+++ src/sample/ReportController.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class ReportController{
+    public String[] contraints;
+
+    public String[] getContraints() {
+        return contraints;
+    }
+
+    public void setContraints(String[] contraints) {
+        this.contraints = contraints;
+    }
+
+    public void generateReport(){
+
+        //code where we generate report from DB based on the constraint/ type of report
+    }
+
+    public String[] getReport(){
+
+    }
+    public void retrieveFromDB( String constraint){
+
+    }
+    public void checkValidity( String constraint){
+
+    }
+}
