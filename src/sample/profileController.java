46eb6b731773fd62fd78130b4424aa7400ecfef2
F5SH123 <39929955+F5SH123@users.noreply.github.com>
2020-05-02 23:13:39 +0300

    Add files via upload

---
+++ src/sample/profileController.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class profileController {
+    private int userID;
+    public void newValueEnterd(String val){
+
+    }
+    public boolean checkValidity(String val){
+        return true;
+    }
+    public void manageProfileSelected(){
+
+    }
+    public void updateProfile(String attribute, String val){
+
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
