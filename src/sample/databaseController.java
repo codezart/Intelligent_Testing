ecf3bfb3d8c49963b1aefce86ad6ee70971e4961
F5SH123 <39929955+F5SH123@users.noreply.github.com>
2020-05-02 23:17:15 +0300

    Add files via upload

---
+++ src/sample/databaseController.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class databaseController {
+    private int userID;
+    private int adminID;
+    public void changeStatus(String val){
+
+    }
+    public void changeID(int id){
+
+    }
+    public void changeName(String name){
+
+    }
+    public void changePassword(int pwd){
+
+    }
+    public void updateDB(){
+
+    }
+    public boolean checkValiditiy(String val){
+        return true;
+    }
+
+
+
+}
